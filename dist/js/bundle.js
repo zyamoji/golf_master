a00b110953a654de53729c9db1943ea3e3625c13
zyamoji <uniquequeue+github@gmail.com>
2019-08-12 04:11:48 +0900

    Add support for reading data from json file.

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -36,32 +36,17 @@
 /******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
 /******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
 /******/ 		}
 /******/ 	};
 /******/
 /******/ 	// define __esModule on exports
 /******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
 /******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
 /******/ 	};
 /******/
 /******/ 	// getDefaultExport function for compatibility with non-harmony modules
@@ -86,6 +71,17 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./course_information.json":
+/*!*********************************!*\
+  !*** ./course_information.json ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("module.exports = {\"courses\":[{\"name\":\"golf_park_tamagawa\",\"holes\":[{\"number\":1,\"par\":3,\"yard_length\":151,\"tee_position\":{\"lng\":139.639445,\"lat\":35.598297},\"cup_position\":{\"lng\":139.638682,\"lat\":35.599348}},{\"number\":2,\"par\":3,\"yard_length\":90,\"tee_position\":{\"lng\":139.637974,\"lat\":35.599366},\"cup_position\":{\"lng\":139.638676,\"lat\":35.599762}}]}]}\n\n//# sourceURL=webpack:///./course_information.json?");
+
+/***/ }),
+
 /***/ "./src/js/app.js":
 /*!***********************!*\
   !*** ./src/js/app.js ***!
@@ -94,7 +90,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _get_position__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./get_position */ \"./src/js/get_position.js\");\n/* harmony import */ var _calc_dist_from_lon_lat__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./calc_dist_from_lon_lat */ \"./src/js/calc_dist_from_lon_lat.js\");\n\n\n\nconst deg_x1 = 139.639445; // tee\nconst deg_y1 = 35.598297;\nconst deg_x2 = 139.638682; // cup\nconst deg_y2 = 35.599348;\n\n// 位置取得ボタン\nconst position_get_btn = document.getElementById('position_get_btn');\n// クリックしたら位置取得\nposition_get_btn.addEventListener(\"click\", async function() {\n    // 現在地の取得の処理を待つためにawait\n    const {ball_lng, ball_lat} = await Object(_get_position__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n    // 現在地からカップまでの距離を計算\n    const {meter, yard} = Object(_calc_dist_from_lon_lat__WEBPACK_IMPORTED_MODULE_1__[\"default\"])(ball_lng, ball_lat, deg_x2, deg_y2);\n    // 計算結果を表示\n    document.getElementById('app').innerHTML = \"カップまでの距離は\" + Math.round(yard) + \"y (\" + Math.round(meter) + \" m)\";\n}, false)\n\n//# sourceURL=webpack:///./src/js/app.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _get_position__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./get_position */ \"./src/js/get_position.js\");\n/* harmony import */ var _calc_dist_from_lon_lat__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./calc_dist_from_lon_lat */ \"./src/js/calc_dist_from_lon_lat.js\");\n/* harmony import */ var _get_course_data__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./get_course_data */ \"./src/js/get_course_data.js\");\n\n\n\n\n\n// 位置取得ボタン\nconst position_get_btn = document.getElementById('position_get_btn');\n// クリックしたら位置取得\nposition_get_btn.addEventListener(\"click\", async function() {\n    // 現在地の取得の処理を待つためにawait\n    const {ball_lng, ball_lat} = await Object(_get_position__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n\n    // 選択されたホールのカップ情報を取得\n    const cup_lng_lat = Object(_get_course_data__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(\"course_select\", \"hole_select\")\n        \n    // 現在地からカップまでの距離を計算\n    const {meter, yard} = Object(_calc_dist_from_lon_lat__WEBPACK_IMPORTED_MODULE_1__[\"default\"])(ball_lng, ball_lat, cup_lng_lat[\"lng\"], cup_lng_lat[\"lat\"]);\n    // 計算結果を表示\n    document.getElementById('app').innerHTML = \"カップまでの距離は\" + Math.round(yard) + \"y (\" + Math.round(meter) + \" m)\";\n}, false)\n\n// コース情報の読み込み\ndocument.addEventListener('DOMContentLoaded', function() {\n    Object(_get_course_data__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(\"course_select\", \"hole_select\")\n    console.log(\"コース情報を読み込みました\")\n}, false)\n\n//# sourceURL=webpack:///./src/js/app.js?");
 
 /***/ }),
 
@@ -107,6 +103,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _get
 
 "use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (function(deg_x1, deg_y1, deg_x2, deg_y2) {\n    console.log('Hello Fon');\n\n    // earth radius(m)\n    const r = 6378137\n    \n    // convert radius from deg\n    const rad_x1 = Math.PI * deg_x1 / 180\n    const rad_y1 = Math.PI * deg_y1 / 180\n    const rad_x2 = Math.PI * deg_x2 / 180\n    const rad_y2 = Math.PI * deg_y2 / 180\n\n    // calc\n    const siny1siny2 = Math.sin(rad_y1) * Math.sin(rad_y2)\n    const cosy1cosy2 = Math.cos(rad_y1) * Math.cos(rad_y2)\n    const deltaX = rad_x2 - rad_x1\n    const cosdx = Math.cos(deltaX)\n    \n    // calc distance(km)\n    const d = r * Math.acos(siny1siny2 + cosy1cosy2 * cosdx)\n    console.log('meter is : ' + d)\n    console.log('yard is : ' + d*1.0936)\n\n    return {'meter': d, 'yard': d*1.0936};\n});\n\n//# sourceURL=webpack:///./src/js/calc_dist_from_lon_lat.js?");
+
+/***/ }),
+
+/***/ "./src/js/get_course_data.js":
+/*!***********************************!*\
+  !*** ./src/js/get_course_data.js ***!
+  \***********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (function(course_id, hole_id) {\n    // コース情報ファイルを指定\n    const course_object = __webpack_require__(/*! ../../course_information.json */ \"./course_information.json\");\n\n    // 選択されたコース情報を取得\n    const selected_course = document.getElementById(course_id).value\n    // 選択されたホール情報を取得\n    const selected_hole = document.getElementById(hole_id).value\n\n    // 選択されたコース/ホールのカップ緯度経度を取得\n    for (let course_info of course_object[\"courses\"]) {\n        // 選択されたコースならホール選択に移る\n        if (course_info[\"name\"] == selected_course) {\n            for (let hole_info of course_info[\"holes\"]) {\n                if (hole_info[\"number\"] == Number(selected_hole)) {\n                    console.log(Number(selected_hole))\n                    return hole_info[\"cup_position\"]\n                }\n            }\n        }\n    }\n});\n\n//# sourceURL=webpack:///./src/js/get_course_data.js?");
 
 /***/ }),
 
